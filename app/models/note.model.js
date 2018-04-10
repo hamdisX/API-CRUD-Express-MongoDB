b82cbfdd11ce262948ce7b0bd32e2ed22082678c
hamdimiled <hamdismiled@gmail.com>
2018-04-10 13:27:17 +0200

    mongo

---
+++ app/models/note.model.js
@@ -0,0 +1,12 @@
+
+
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    title: String,
+    content: String
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Note', NoteSchema);
--- server.js
+++ server.js
@@ -11,10 +11,28 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url)
+.then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
 });
+
+// Require Notes routes
+require('./app/routes/note.routes.js')(app);
 
 // listen for requests
 app.listen(3000, () => {
